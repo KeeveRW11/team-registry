2565d5db3ab6f5148495f703e22b164a41fd2f43
KeeveRW11 <keevewhyte+github@gmail.com>
2022-09-03 13:05:44 -0400

    test fail

--- index.js
+++ index.js
@@ -7,10 +7,49 @@ const prompts = () => {
     [
         {
             type: 'input',
-            name: 'name',
-            message: 'What is the name of your employee? ()Required',
-            validate: nameInput => {
-                if (nameInput) {
+            name: 'managerName',
+            message: 'What is the name of the manager? ()Required',
+            validate: managerNameInput => {
+                if (managerNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of the employee!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerID',
+            message: 'Manager,please provide your ID number to continue ()Required',
+            validate: manageIDInput => {
+                if (manageIDInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your ID number!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: 'Manager,please provide your email address to continue ()Required',
+            validate: managerEmailInput => {
+                if (managerEmailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter email address!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: 'What is your office? ()Required',
+            validate: officeInput => {
+                if (officeInput) {
                     return true;
                 } else {
                     console.log('Please enter the name of the employee!');
---
+++ src/page-template.js
@@ -0,0 +1,40 @@
+module.exports = templateData  => {
+    console.log(templateData);
+    const { projects, about, ...header } = templateData;
+  
+  
+    return `  
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    </head>
+  
+      
+    <body>
+      <header>
+        <div class="container flex-row justify-space-between align-center py-3">
+          <h1 class="page-title text-secondary bg-dark py-2 px-3"></h1>
+          <nav class="flex-row">
+            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" 
+              header.github
+            }">GitHub</a>
+          </nav>
+        </div>
+      </header>
+      <main class="container my-5">
+
+      </main>
+      <footer class="container text-center py-3">
+        <h3 class="text-dark">&copy; </h3>
+      </footer>
+    </body>
+    </html>
+    `;  
+  };
